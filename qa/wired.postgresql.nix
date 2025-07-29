7a537e60cd429161cc0aa25c17703cd38ad733f2
Leandro Alves <leandroprogramadoruxdesign@gmail.com>
2025-07-29 19:04:03 -0300

    usuario do postgres sem initdb no PATH (????)

--- qa/wired.postgresql.nix
+++ qa/wired.postgresql.nix
@@ -13,9 +13,10 @@ pkgs.mkShell {
     sudo rm -rf /usr/local/pgsql
     sudo mkdir /usr/local/pgsql
     sudo chown postgres /usr/local/pgsql
-    sudo su postgres -c "initdb -D /usr/local/pgsql/data"
+    sudo su postgres -c "export PATH=$PATH; echo ''$PATH > /usr/local/pgsql/teste-path.txt"
+    sudo su postgres -c "export PATH=$PATH; initdb -D /usr/local/pgsql/data"
  
-    sudo su postgres -c "postgres -D /usr/local/pgsql/data >/usr/local/pgsql/data/logfile 2>&1 &"
+    sudo su postgres -c "export PATH=$PATH; postgres -D /usr/local/pgsql/data >/usr/local/pgsql/data/logfile 2>&1 &"
     sleep 5
 
     sudo su postgres -c "psql --command=\"CREATE DATABASE rinha_haskell;\" --port=5434"
